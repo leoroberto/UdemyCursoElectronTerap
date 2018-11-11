c18783fad6ea17796a5a221c08e5325effe542ae
Leonardo Farias <leonardo.r.farias@avanade.com>
2018-11-11 20:23:34 -0300

    Subida da terceira aplicação do curso de electron da Udemy

---
+++ app/chronotray.js
@@ -0,0 +1,42 @@
+const electron = require('electron');
+const {Tray, app, Menu} = electron;
+
+const contextMenu = Menu.buildFromTemplate([
+    {
+        label: 'Sair',
+        click: () => {
+            app.quit();
+        }
+    }
+]);
+
+class ChronoTray extends Tray{
+    constructor(iconPath, mainWindow){
+        super(iconPath); // Aqui estou chamando o construtor do Tray
+        this.mainWindow = mainWindow;
+        this.on('click', this.onClick.bind(this));
+        this.setToolTip('Esta é uma aplicação Electron');
+        this.setContextMenu(contextMenu);
+    }
+
+    onClick(event, bounds){
+        //coordenadas do ícone da bandeja
+        const{x, y} = bounds;
+        //diensões da janela(largura e altura)
+        const {width, height} = this.mainWindow.getBounds();
+
+        if(this.mainWindow.isVisible()){
+            this.mainWindow.hide();
+        }else{
+            this.mainWindow.setBounds({
+                x: x >= 400 ? x - Math.floor(width / 2) : x,
+                y: y >= 300 ? y - height : y,
+                width,
+                height
+            });
+            this.mainWindow.show();
+        }
+    }
+}
+
+module.exports = ChronoTray;
